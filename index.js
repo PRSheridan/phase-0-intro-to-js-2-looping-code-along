f9abf281d2a69ba4a5327d26f1083252f0eaea35
Phil <philrsheridan@gmail.com>
2023-09-07 12:41:07 -0400

    Completed 3 tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    let cardsToSend = [];
+
+    for(let i = 0; i < names.length; i++){
+        cardsToSend.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return cardsToSend;
+}
+
+function countDown(downFrom){
+    while (downFrom >= 0){
+        console.log(downFrom);
+        downFrom--;
+    }
+}
+
